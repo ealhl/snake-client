1058a4c7213e9e50ca073e5436cd52d7bc93559e
ealhl <yayt.tuk@gmail.com>
2023-12-15 20:36:17 -0800

    add client.js

--- play.js
+++ play.js
@@ -1,26 +1,26 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect  = () => {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541,
   });
 
   // interpret incoming data as text
-  
+
   conn.on("event name", () => {
     // code that does something
   });
   conn.on("connect", () => {
     // code that does something when the connection is first established
   });
-  conn.on('data', (data) => {
+  conn.on("data", (data) => {
     console.log(data.toString());
   });
 
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
+
+module.exports = { connect };
