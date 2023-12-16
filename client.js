885b2406414416b53268cc9f2181dd182b5d5f7e
ealhl <yayt.tuk@gmail.com>
2023-12-15 22:01:57 -0800

    add move uplr and comment out:

--- client.js
+++ client.js
@@ -1,3 +1,50 @@
-const { connect } = require("./play");
+const net = require("net");
 
-connect();
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+
+  conn.on("event name", () => {
+    // code that does something
+  });
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("Successfully connected to game server");
+    conn.write("Name: LHL");
+  });
+
+  // conn.on("connect", () => {
+  //   // code that does something when the connection is first established
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 5000);
+  //   setTimeout(() => {
+  //     conn.write("Move: left");
+  //   }, 6000);
+  //   setTimeout(() => {
+  //     conn.write("Move: down");
+  //   }, 7000);
+  //   setTimeout(() => {
+  //     conn.write("Move: down");
+  //   }, 8000);
+  //   setTimeout(() => {
+  //     conn.write("Move: down");
+  //   }, 9000);
+  //   setTimeout(() => {
+  //     conn.write("Move: right");
+  //   }, 10000);
+  // });
+
+  conn.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,26 +1,4 @@
-const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect  = () => {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-
-  conn.on("event name", () => {
-    // code that does something
-  });
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-  });
-  conn.on("data", (data) => {
-    console.log(data.toString());
-  });
-
-  return conn;
-};
-
-
-module.exports = { connect };
+console.log(connect);
+connect.connect();
