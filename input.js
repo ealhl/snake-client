08ed53a918f82b47ccc64e2ed1af46a4ce626288
ealhl <yayt.tuk@gmail.com>
2023-12-15 22:51:23 -0800

    add input js

--- input.js
+++ input.js
@@ -1,13 +1,29 @@
+let connection;
+
 const handleUserInput = function (data) {
+  // process.stdout.write(".");
   // your code here
-  process.stdout.write(".");
+  if (data === "w") {
+    connection.write("Move: up");
+  }
+  if (data === "a") {
+    connection.write("Move: left");
+  }
+  if (data === "s") {
+    connection.write("Move: down");
+  }
+  if (data === "d") {
+    connection.write("Move: right");
+  }
+
   if (data === "\u0003") {
     process.exit();
   }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
