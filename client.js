3a4866d651502d119a718f6e67a0787918b256ba
ealhl <yayt.tuk@gmail.com>
2023-12-15 23:01:21 -0800

    add constant

--- client.js
+++ client.js
@@ -1,10 +1,13 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = () => {
+  console.log(typeof(IP));
+  console.log(typeof(PORT));
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
